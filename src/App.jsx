462ab964ec33f6b692afd13e9b3ee42b5c478232
Jaguar Deva Nanggalasakti Oktavian <114300789+jaguardeva@users.noreply.github.com>
2023-08-13 20:40:35 +0700

    delete browserouter at app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import MyProject from "./pages/MyProject";
 import Contact from "./pages/Contact";
-import Card from "./components/Projects/Card";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/project" element={<MyProject />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/card" element={<Card />} />
       </Routes>
     </Navbar>
   );
